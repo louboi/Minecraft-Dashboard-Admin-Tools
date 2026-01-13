0d0ac8cc6589a5ed7de816f3ba6da5723bf48e4b
Louboi <louisjeanes4@protonmail.com>
2026-01-13 19:14:40 +0000

    Final brief edit

--- Backend/Database_Con.py
+++ Backend/Database_Con.py
@@ -23,27 +23,3 @@ def User_only(Username):
 
 Username = str(input("What is the username? "))
 print(User_only(Username))
-
-'''
-# ---- ADD A USER TO THE DATABASE ---- #
-# Get the UUID of a specified user
-username = str(input("What is the username? ")) # Get the username
-get_url = "https://api.mojang.com/users/profiles/minecraft/" + username  # Create a url for getting the UUID
-api_response = req.get(get_url) # Make the request and get the response
-
-# Process the JSON, and handle any unexpected errors
-if api_response.status_code == 200:
-    json_response = api_response.json()
-    UserID = json_response.get("id")
-else:
-    print("Try again")
-
-# Define data set to be added
-user_info = [
-    (username, UserID),
-]
-
-# Add the data and commit the changes to the file
-cur.executemany("INSERT INTO users VALUES(?, ?)", user_info)
-con.commit()
-'''
